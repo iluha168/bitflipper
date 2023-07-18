68b2e0c0de0405b00d9aa2438919842e08155af9
iluha168 <illya.chernyshov888@gmail.com>
2023-07-18 16:19:44 +0300

    Long options, RAM implementation, header skip

--- README.md
+++ README.md
@@ -7,11 +7,14 @@ Compile `main.c` and execute in your preffered system with arguments listed belo
 
 ## Usage
 ### Arguments
-- `-n <number>` set amount of bits to flip in a file. **0 if not provided.**
-- `-s <number>` sets the seed for random generator. Random generator is responsible for picking the bits to flip. 0 if not provided.
-- `-s time` sets the seed from current system time
-- `-h` shows the help message
-- `-v` enables verbose mode
+- `-n, --amount <number>` set amount of bits to flip in a file. **0 if not provided.**
+- `-s, --seed <number>` sets the seed for random generator. Random generator is responsible for picking the bits to flip. **0 if not provided.**
+- `-s, --seed time` sets the seed from current system time.
+- `-h, --help` shows the help message.
+- `-v, --verbose` enables verbose mode.
+- `--disk` write changes to the disk immediately as they appear.
+- `--ram` make changes in the RAM copy of the file, then rewrite the file once with modified data.
+- `--skip <number>` amount of **bytes** from the beginning of the file the bitflipper will not affect.
 
 ### Examples
 `bitflipper ./image.bmp -n 1000 -stime`
@@ -22,7 +25,4 @@ Interesting behavior can be seen if a command is executed **twice** with the sam
 
 `bitflipper ./image2.bmp -n 10000 -s 1234`
 
-On the first execution the file is going to get partially or fully corrupted, but on the second run the file is going to return to it's initial state, since the bits flipped are going to be exactly the same as before. 
-
-This essentially encrypts the file. To decrypt it you need to know the number of bits flipped and the seed used to generate the sequence of bits to flip.
-
+On the first execution the file is going to get partially or fully corrupted, but on the second run the file is going to return to it's initial state, since the bits flipped are going to be exactly the same as before. This essentially decrypts the file.
--- main.c
+++ main.c
@@ -23,29 +23,56 @@ long randl(){
 
 int main(int argc, char** argv){
     int toflip = 0;
-    unsigned char verbose = 0;
+    int verbose_flag = 0;
+    int implementation = 0;
+    unsigned long headerSize = 0;
+    char* buffer;
 
+    const struct option long_options[] = {
+    /*0*/{"verbose", no_argument, &verbose_flag, 1},
+    /*1*/{"help"   , no_argument, NULL, 'h'},
+    /*2*/{"seed"   , required_argument, NULL, 's'},
+    /*3*/{"amount" , required_argument, NULL, 'n'},
+    /*4*/{"disk"   , no_argument, &implementation, 0},
+    /*5*/{"ram"    , no_argument, &implementation, 1},
+    /*6*/{"skip"   , required_argument, NULL, 0},
+    /*7*/{0, 0, NULL, 0}
+    };
+
+    int long_option_i = 0;
     char res;
-    while((res = getopt(argc, argv, "hn:vs:")) != -1){
+    while((res = getopt_long(argc, argv, "hn:vs:", long_options, &long_option_i)) != -1){
         switch (res){
+        case 0:
+            if(long_options[long_option_i].flag != NULL) break;
+            switch(long_option_i){
+                case 6: //skip
+                    headerSize = strtoul(optarg, NULL, 10);
+                break;
+            }
+        break;
         case 'n':
             toflip = strtoul(optarg, NULL, 10);
         break;
         case 'v':
-            verbose = 1;
+            verbose_flag = 1;
         break;
         case 's':
             if(strcmp(optarg, "time") == 0)
                 srandl(time(NULL));
             else srandl(strtoul(optarg, NULL, 10));
         break;
+        case '?':
         case 'h':
-            printf("Usage:\nbitflipper [-n N] [-v] [-s SEED] [-h]\n"
-                "\t-h\tshow this message and exit(0)\n"
-                "\t-v\tenables verbose mode\n"
-                "\t-s\tset seed to generate positions of bits to flip (default: 0)\n"
-                "\t-stime\tset seed from current system time\n"
-                "\t-n\tset amount of bits to flip in a file (default: 0)\n"
+            printf("Usage:\nbitflipper [OPTION]… [FILENAME]\n"
+                "\t-h, --help\tshow this message and exit(0)\n"
+                "\t-v, --verbose\tenables verbose mode\n"
+                "\t-s, --seed\tset seed to generate positions of bits to flip (default: 0)\n"
+                "\t-s, --seed time\tset seed from current system time\n"
+                "\t-n, --amount\tset amount of bits to flip in a file (default: 0)\n"
+                "\t--disk\twrite changes directly into the file (best for low --amount)\n"
+                "\t--ram\tmake changes in the memory, then rewrite the file (best for high --amount)\n"
+                "\t--skip\t set header size - amount of bytes from the beginning of the file that will not be affected.\n"
             );
         exit(0);
         }
@@ -53,25 +80,41 @@ int main(int argc, char** argv){
     }
 
     const char* filename = argv[optind];
-    if(verbose){
+    if(verbose_flag){
         printf("Bits to flip: %d\n", toflip);
         printf("File to flip: %s\n", filename);
         printf("Seed: %lu\n", seed);
+        printf("Header size: %d\n", headerSize);
     }
     //actual code
     FILE *f = fopen(filename, "r+");
     if(f == NULL) exit(-2);
     fseek(f, 0, SEEK_END);
-    long filesize = ftell(f);
+    const long fsizeWOHeader = ftell(f)-headerSize;
+    if(implementation == 1){
+        buffer = malloc(fsizeWOHeader);
+        fseek(f, headerSize, SEEK_SET);
+        for(size_t read = 0; read < fsizeWOHeader; read += fread(buffer+read, 1, fsizeWOHeader-read, f));
+    }
     while(toflip --> 0){
-        long pos = randl()%filesize;
-        fseek(f, pos, SEEK_SET);
-        char byte;
-        fread(&byte, 1, 1, f);
-        byte = byte ^ (1 << (rand()%8));
-        fseek(f, pos, SEEK_SET);
-        fwrite(&byte, 1, 1, f);
-        if(verbose) printf("Flipping... Left: %d        \r", toflip);
+        long pos = randl()%fsizeWOHeader;
+        const char bitmask = 1 << (rand()%8);
+        if(implementation == 0){
+            fseek(f, pos+headerSize, SEEK_SET);
+            char byte;
+            fread(&byte, 1, 1, f);
+            byte ^= bitmask;
+            fseek(f, pos+headerSize, SEEK_SET);
+            fwrite(&byte, 1, 1, f);
+        } else if(implementation == 1){
+            buffer[pos] ^= bitmask;
+        }
+        if(verbose_flag) printf("Flipping... Left: %d        \r", toflip);
+    }
+    if(implementation == 1){
+        fseek(f, headerSize, SEEK_SET);
+        for(size_t written = 0; written < fsizeWOHeader; written += fwrite(buffer+written, 1, fsizeWOHeader-written, f));
+        free(buffer);
     }
     fclose(f);
 }
